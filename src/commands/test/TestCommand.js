c39460aaad1939ae8780c1c19f47f497c7c442eb
Peace <Debitor.TooLP@gmx.de>
2021-09-22 20:07:43 +0200

    changed event and command creation

--- src/commands/test/TestCommand.js
+++ src/commands/test/TestCommand.js
@@ -1,11 +1,7 @@
-const BaseCommand = require('../../utils/structures/BaseCommand');
+module.exports = {
+	command: 'test',
 
-module.exports = class TestCommand extends BaseCommand {
-  constructor() {
-    super('test', 'testing', []);
-  }
-
-  async run(client, message, args) {
-    message.channel.send('Test command works');
-  }
-}
+	async run(client, message, args) {
+		message.channel.send('Test command works');
+	}
+};
--- src/events/message/MessageCreateEvent.js
+++ src/events/message/MessageCreateEvent.js
@@ -1,21 +1,14 @@
-const BaseEvent = require('../../utils/structures/BaseEvent');
+module.exports = {
+	name: 'messageCreate',
 
-module.exports = class MessageEvent extends BaseEvent {
-  constructor() {
-    super('messageCreate');
-  }
-  
-  async run(client, message) {
-    if (message.author.bot) return;
-    if (message.content.startsWith(client.prefix)) {
-      const [cmdName, ...cmdArgs] = message.content
-      .slice(client.prefix.length)
-      .trim()
-      .split(/\s+/);
-      const command = client.commands.get(cmdName);
-      if (command) {
-        command.run(client, message, cmdArgs);
-      }
-    }
-  }
-}
+	async run(client, message) {
+		if (message.author.bot) return;
+		if (message.content.startsWith(client.prefix)) {
+			const [ cmdName, ...cmdArgs ] = message.content.slice(client.prefix.length).trim().split(/\s+/);
+			const command = client.commands.get(cmdName);
+			if (command) {
+				command.run(client, message, cmdArgs);
+			}
+		}
+	}
+};
