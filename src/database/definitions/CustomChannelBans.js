b8a8c5dd163a8f6b96e0c6e3730a5a18b63c6602
Peace <Debitor.TooLP@gmx.de>
2021-09-28 13:37:57 +0200

    added CustomChannelBans definition

---
+++ src/database/definitions/CustomChannelBans.js
@@ -0,0 +1,23 @@
+const DataTypes = require('sequelize');
+
+module.exports = {
+	name: 'CustomChannelBans',
+	table: {
+		userId: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	},
+	associations: [
+		{
+			type: 'belongsTo',
+			table: 'CustomChannels',
+			options: {
+				onDelete: 'CASCADE',
+				foreignKey: {
+					allowNull: false,
+				},
+			},
+		},
+	],
+};
