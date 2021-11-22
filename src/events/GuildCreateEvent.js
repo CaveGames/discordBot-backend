7e680e7d40d67920bc08df37916b68982d84f76d
Peace <Debitor.TooLP@gmx.de>
2021-11-22 17:19:18 +0100

    added automatic add all users to database on botJoin

--- src/events/GuildCreateEvent.js
+++ src/events/GuildCreateEvent.js
@@ -1,4 +1,4 @@
-const { GuildData } = require('../database').models;
+const { GuildData, UserData } = require('../database').models;
 const config = require('../../config.json');
 
 module.exports = {
@@ -8,9 +8,21 @@ module.exports = {
 		const logs = await guild.fetchAuditLogs({ type: 'BOT_ADD', limit: 1 });
 		const owner = logs.entries.first().executor;
 
-		GuildData.create({
+		await GuildData.create({
 			guildId: guild.id,
 			ownerId: owner.id,
+		});
+
+		const members = guild.members.cache;
+		members.forEach(member => {
+			if (member.user.bot) return;
+
+			UserData.create({
+				userId: member.user.id,
+				guildId: guild.id,
+				firstJoinDate: new Date(member.joinedTimestamp),
+				isVerified: false,
+			});
 		});
 	},
 };
