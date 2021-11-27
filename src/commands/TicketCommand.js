cd95aca58cb143148dabda507349feee7a8d8977
Peace <Debitor.TooLP@gmx.de>
2021-11-27 19:49:15 +0100

    added button if only one ticket category exists

--- src/commands/TicketCommand.js
+++ src/commands/TicketCommand.js
@@ -11,37 +11,67 @@ module.exports = {
 		const messages = await channel.messages.fetch();
 		messages.forEach(msg => msg.delete());
 
-		const options = [];
-		config.tickets.categories.forEach((category, i) => {
-			options.push({
-				label: category.name,
-				value: i.toString(),
-				description: category.description,
-				emoji: category.emoji != null ? emoji.getUnicode(category.emoji) : null,
+		if (config.tickets.categories.length > 1) {
+			const options = [];
+			config.tickets.categories.forEach((category, i) => {
+				options.push({
+					label: category.name,
+					value: i.toString(),
+					description: category.description,
+					emoji: category.emoji != null ? emoji.getUnicode(category.emoji) : null,
+				});
 			});
-		});
 
-		channel.send({
-			embeds: [
-				{
-					title: 'Ticket System',
-					description: 'Wähle hier das Ticket aus, welches du erstellen möchtest',
-					color: config.accentColor,
-				},
-			],
-			components: [
-				{
-					type: 1,
-					components: [
-						{
-							type: 3,
-							placeholder: 'Wähle eine Kategorie',
-							custom_id: 'ticket_select',
-							options: options,
-						},
-					],
-				},
-			],
-		});
+			channel.send({
+				embeds: [
+					{
+						title: 'Ticket System',
+						description: 'Wähle hier das Ticket aus, welches du erstellen möchtest',
+						color: config.accentColor,
+					},
+				],
+				components: [
+					{
+						type: 1,
+						components: [
+							{
+								type: 3,
+								placeholder: 'Wähle eine Kategorie',
+								custom_id: 'ticket_select',
+								options: options,
+							},
+						],
+					},
+				],
+			});
+		}
+		else if (config.tickets.categories.length == 1) {
+			channel.send({
+				embeds: [
+					{
+						title: 'Ticket System',
+						description: 'Klicke auf den Knopf um ein Ticket zu öffnen',
+						color: config.accentColor,
+					},
+				],
+				components: [
+					{
+						type: 1,
+						components: [
+							{
+								type: 2,
+								style: 2,
+								label: config.tickets.categories[0].name,
+								emoji:
+									config.tickets.categories[0].emoji != null
+										? emoji.getUnicode(config.tickets.categories[0].emoji)
+										: null,
+								custom_id: 'ticket_select',
+							},
+						],
+					},
+				],
+			});
+		}
 	},
 };
