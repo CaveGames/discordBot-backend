960c5ae6735f82c0c0806d1bd5c7c48925d20641
Peace <Debitor.TooLP@gmx.de>
2022-06-01 14:02:50 +0200

    added bot channel welcome message

--- src/cron/botChannel.js
+++ src/cron/botChannel.js
@@ -16,10 +16,38 @@ async function clear() {
 		channel.bulkDelete(100);
 		messages = channel.messages.fetch();
 	}
+
+	init();
 }
 
 async function init() {
-	// TODO
+	const guild = global.client.guilds.cache.get(config.guildId);
+	const channel = guild.channels.cache.get(config.botChannelId);
+
+	channel.send({
+		embeds: [
+			{
+				title: 'Befehlige deine Untertanen',
+				description:
+					'Über Nachrichten in diesem Channel kannst du die Bots befehligen.\n\n' +
+					'**Musik-Bot**\n' +
+					'**► `m!help`**\n' +
+					'Sehe alle Befehle für den _Dicken fetten Jens_, unseren Musik-Bot.\n\n' +
+					'**Custom Channels**\n' +
+					'**► `!cc`**\n' +
+					'Lasse dir Infos über den aktuellen Custom Channel anzeigen.\n\n' +
+					'**► `!cc private|public`**\n' +
+					'Schalte den aktuellen Custom Channel auf privat oder öffentlich.\n\n' +
+					'**► `!cc hide|show`**\n' +
+					'Mache den aktuellen Custom Channel (un)sichtbar.\n\n' +
+					'**► `!cc kick <@Benutzer>`**\n' +
+					'Werfe einen Nutzer aus dem Custom Channel.\n\n' +
+					'**► `!cc ban|unban`**\n' +
+					'(Ent)banne einen Benutzer in dem Custom Channel.\n\n',
+				color: config.accentColor,
+			},
+		],
+	});
 }
 
 module.exports = { clear, init };
